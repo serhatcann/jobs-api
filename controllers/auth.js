b3733bef59b7a97e91f87f1bd7b7816cc67304d2
Serhat Can <serhatcan1996@gmail.com>
2023-02-13 17:01:13 +0300

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
+
+const register = async (req, res) => {
+	const user = await User.create({ ...req.body });
+
+	res
+		.status(StatusCodes.CREATED)
+		.json({ user: { name: user.name }, token: user.createJWT() });
+};
+
+const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		throw new BadRequestError('Please provide email and password');
+	}
+
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new UnauthenticatedError('Invalid Credentials');
+	}
+
+	const isPasswordCorrect = await user.comparePassword(password);
+	if (!isPasswordCorrect) {
+		throw new UnauthenticatedError('Invalid Credentials');
+	}
+
+	res
+		.status(StatusCodes.OK)
+		.json({ user: { name: user.name }, token: user.createJWT() });
+};
+
+module.exports = {
+	register,
+	login,
+};
